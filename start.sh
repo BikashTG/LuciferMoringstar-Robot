4fb52bc11482457f70337e8bdded818f39efcaee
ɮɨӄǟֆɦ ʍǟɨȶʏ <118190786+BikashTG@users.noreply.github.com>
2022-12-03 19:37:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/BikashTG/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
-fi
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/BikashTG/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 cd /LuciferMoringstar-Robot
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting LuciferMoringstar RoBot...."
+echo "Starting Bot Please Wait..."
 python3 main.py
